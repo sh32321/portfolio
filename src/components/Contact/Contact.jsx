7927cf1e8e0fc63a1fa6301e02679900b3cf0631
sh32321 <sh32321@gmail.com>
2022-03-09 14:53:27 -0800

    contact section modified

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,12 +35,17 @@ const Contact = () => {
     <div className="form-container" id="contact">
       <form onSubmit={sendEmail} ref={form}>
         <div className="line"></div>
+        <p className="contact-title">Contact me</p>
+        <div className="line"></div>
+        <p className="drop-line">
+          Drop me in line if you would like to work with me
+        </p>
+        <p className="email">📩 sh32321@gmail.com</p>
         {success ? (
           <p className="success">{success}</p>
         ) : (
-          <p className="contact-title">Contact me</p>
+          <p className="drop-line">Or send me the email from here :)</p>
         )}
-        <div className="line"></div>
 
         <label>
           <span>Name</span>
